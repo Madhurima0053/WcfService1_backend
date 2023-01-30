a1fe34b1e134a2ab0fd068fb7538af741080c4e0
2134164 <2134164@LTIN289725>
2023-01-30 16:53:08 +0530

    Add project files.

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string InsertUserDetails (UserDetails userInfo);
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class UserDetails
+    {
+        string name = String.Empty;
+        string address = String.Empty;
+        string pin= String.Empty;
+        string phone = String.Empty;
+        string email = String.Empty;
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name= value; }
+        }
+
+        [DataMember]
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        [DataMember]
+        public string Pin
+        {
+            get { return pin; }
+            set { pin = value; }
+        }
+        [DataMember]
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string InsertUserDetails(UserDetails userInfo)
+        {
+            string Message;
+            SqlConnection con = new SqlConnection("Data Source=LTIN289725\\SQLEXPRESS;Initial Catalog=Registration;Integrated Security=True;MultipleActiveResultSets=True;Encrypt=False;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into RegistrationTable (Name, Address, Pin, Phone, Email) values( @Name , @Address , @Pin , @Phone , @Email)", con);
+            cmd.Parameters.AddWithValue("Name", userInfo.Name);
+            cmd.Parameters.AddWithValue("Address", userInfo.Address);
+            cmd.Parameters.AddWithValue("Pin", userInfo.Pin);   
+            cmd.Parameters.AddWithValue("Phone", userInfo.Phone);
+            cmd.Parameters.AddWithValue("Email", userInfo.Email);
+            int result = cmd.ExecuteNonQuery();
+            if(result == 1)
+            {
+                Message = userInfo.Name + "Details inserted successfully";
+            }
+            else
+            {
+                Message = userInfo.Name + "Details not inserted successfully";
+            }
+            con.Close();
+            return Message;
+        }
+    }
+}
